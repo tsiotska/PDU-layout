358fba6cea5683d5513af7b5ece8d73c3d530dc9
big.siski <vitaliytsisaruk@gmail.com>
2020-04-07 18:25:43 +0300

    Overview refactored

--- src/containers/Overview/Overview.js
+++ src/containers/Overview/Overview.js
@@ -14,25 +14,31 @@ export default class Overview extends Component {
 				</div>
 
 				<div className="table">
+
 					{MockOutlets.data.map((outlet, outletInx) => (
 						<div className="column">
 
 							<div className="row">
+								<div className="label"> Phase</div>
 								<div className="row">
 									<div className="Box">{outletInx + 1}</div>
-									<div className="column">
-										<div className="label">Voltage</div>
-										<div>{outlet.Phases.Voltage}</div>
+
+									<div className="row">
+										<div className="column">
+											<div className="label">Voltage</div>
+											<div>{outlet.Phases.Voltage}</div>
+										</div>
+										<div className="column">
+											<div className="label">Current</div>
+											<div> {outlet.Phases.Current}</div>
+										</div>
 									</div>
 
-									<div className="column">
-										<div className="label">Current</div>
-										<div> {outlet.Phases.Current}</div>
-									</div>
 								</div>
 							</div>
 
 							<div className="row ">
+								<div className="label"> Output</div>
 								<div className="row">
 									{outlet.Outputs.map((output, outputInx) => (
 										<div className="column">
@@ -43,23 +49,36 @@ export default class Overview extends Component {
 							</div>
 
 							<div className="row">
-								{outlet.Outputs.map((output) => (
-									<div className="column">
-										{Object.entries(output).map((field) =>
-											(<div className="label">
-												<div className="field">
-													{field[1]}
-												</div>
-											</div>))
-										}
-									</div>))}
-							</div>
 
-						</div>))}
+								<div className="column">
+									<div className="label"> State</div>
+									<div className="label"> Voltage</div>
+									<div className="label"> Current</div>
+									<div className="label"> Active power</div>
+								</div>
+
+								<div className="row">
+									{outlet.Outputs.map((output) => (
+										<div className="column">
+											{Object.entries(output).map((value) =>
+												(
+													<div className="value">
+														{value[1]}
+													</div>
+												))
+											}
+										</div>))}
+								</div>
+
+							</div>
+						</div>
+					))}
 				</div>
 
 				<SystemLayout/>
+
 			</div>
+
 		);
 	}
 }
