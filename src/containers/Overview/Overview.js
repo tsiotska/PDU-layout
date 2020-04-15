a967e29a92dc81e8fa7f0da7f1ce8738a6519d99
big.siski <vitaliytsisaruk@gmail.com>
2020-04-15 12:31:09 +0300

    Ground completed

--- src/containers/Overview/Overview.js
+++ src/containers/Overview/Overview.js
@@ -3,6 +3,8 @@ import './Overview.less';
 import {MockOutlets} from '../../constants';
 import SystemLayout from './SystemLayout/SystemLayout';
 import Output from '../../assets/output.svg';
+import Ground from '../../assets/ground.svg';
+
 
 export default class Overview extends Component {
 	render() {
@@ -14,66 +16,74 @@ export default class Overview extends Component {
 
 				<div className="table">
 
-					{MockOutlets.data.map((outlet, outletInx) => (
-						<div className="column">
-
-							<div className="row">
-								<div className="label"> Phase</div>
+					<div className="grid">
+						{MockOutlets.data.map((outlet, outletInx) => (
+							<div className="column">
 
 								<div className="row">
-									<div className="Box">{outletInx + 1}</div>
+									<div className="label"> Phase</div>
 
 									<div className="row">
-										<div className="column">
-											<div className="label"> Voltage</div>
-											<div>{outlet.Phases.Voltage}</div>
-										</div>
-										<div className="column">
-											<div className="label"> Current</div>
-											<div> {outlet.Phases.Current}</div>
+										<div className="Box">{outletInx + 1}</div>
+
+										<div className="row">
+											<div className="column">
+												<div className="label"> Voltage</div>
+												<div>{outlet.Phases.Voltage}</div>
+											</div>
+											<div className="column">
+												<div className="label"> Current</div>
+												<div> {outlet.Phases.Current}</div>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
 
-							<div className="row ">
-								<div className="label"> Output</div>
-								<div className="row">
-									{outlet.Outputs.map((output, outputInx) => (
-										<div className="column">
+								<div className="row ">
+									<div className="label"> Output</div>
+									<div className="row">
+										{outlet.Outputs.map((output, outputInx) => (
+											<div className="column">
 
-											<div className="icon">
-												<Output alt="output" className={output.State}/>
-											</div>
-
-											<p>{'Output_' + outputInx}</p></div>
-									))}
-								</div>
-							</div>
-
-							<div className="row">
-
-								<div className="column">
-									<div className="label"> State</div>
-									<div className="label"> Voltage</div>
-									<div className="label"> Current</div>
-									<div className="label"> Active power</div>
-								</div>
-
-								<div className="row">
-									{outlet.Outputs.map((output) => (
-										<div className="column">
-											{Object.entries(output).map((value) => (
-												<div className={"value " + (value[0] === "State" ? value[1] : "")}>
-													{value[1]}
+												<div className="icon">
+													<Output alt="output" className={output.State}/>
 												</div>
-											))}
-										</div>))}
+
+												<p>{'Output_' + outputInx}</p></div>
+										))}
+									</div>
 								</div>
 
+								<div className="row">
+
+									<div className="column">
+										<div className="label"> State</div>
+										<div className="label"> Voltage</div>
+										<div className="label"> Current</div>
+										<div className="label"> Active power</div>
+									</div>
+
+									<div className="row">
+										{outlet.Outputs.map((output) => (
+											<div className="column">
+												{Object.entries(output).map((value) => (
+													<div className={"value " + (value[0] === "State" ? value[1] : "")}>
+														{value[1]}
+													</div>
+												))}
+											</div>))}
+									</div>
+
+								</div>
 							</div>
+						))}
+					</div>
+
+					<div className="groundContainer groundActive"> {/*Toggle groundActive and groundDisabled*/}
+						<div className="iconContainer">
+						<Ground className="icon"/>
 						</div>
-					))}
+					</div>
 				</div>
 
 				<SystemLayout/>
